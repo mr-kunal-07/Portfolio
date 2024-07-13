515a37d2eae7e469a5affd415e2feb957784ab05
Kunal Jadhav <kunaljadhav2305@gmail.com>
2024-07-13 21:34:09 +0530

    Org

---
+++ src/constants.js
@@ -0,0 +1,170 @@
+import { HTML, CSS, JavaScript, ReactJS, Redux, NextJS, GSAP, SASS, Bootstrap, TailwindCSS, MaterialUI, NodeJS, ExpressJS, MongoDB, MySQL, Firebase, C, CPP, Java, Python, Git, Github, Azure, VSCode, Postman, Figma } from './assets/export';
+// import { FoundationalAI, Webexcep, GirlScript, Hack2Skill } from './assets/export';
+import { CSMU } from './assets/export';
+import { StudyZone } from './assets/export';
+
+
+export const Bio = {
+  name: "Kunal Jadhav",
+  roles: ["Frontend Developer", "Freelancer"],
+  description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  resume: "https://drive.google.com/file/d/1Sbr1Oeferv47EEeCJItwiqd-A_fk3BAj/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/kunaljadhav4295/",
+  github: "https://github.com/Itsbitscoder/",
+  leetcode: "https://leetcode.com/u/KunalJadhav42/",
+  geeksforgeeks: "https://www.geeksforgeeks.org/user/kunaljadhav4295/",
+  X: "https://x.com/Kunaljadhav02",
+  whatsapp: "https://wa.me/9920655685?text=Hi Anshul! I'd like to know more about your services."
+};
+
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", image: HTML },
+      { name: "CSS", image: CSS },
+      { name: "JavaScript", image: JavaScript },
+      { name: "React JS", image: ReactJS },
+      { name: "Redux", image: Redux },
+      // { name: "Next JS", image: NextJS },
+      { name: "GSAP", image: GSAP },
+      // { name: "SASS", image: SASS },
+      { name: "Bootstrap", image: Bootstrap },
+      { name: "Tailwind CSS", image: TailwindCSS },
+      { name: "Material UI", image: MaterialUI }
+    ]
+  },
+
+  {
+    title: "Backend",
+    skills: [
+      { name: "Node JS", image: NodeJS },
+      // { name: "Express JS", image: ExpressJS },
+      { name: "MongoDB", image: MongoDB },
+      { name: "MySQL", image: MySQL },
+      { name: "Firebase", image: Firebase }
+    ]
+  },
+
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", image: C },
+      { name: "C++", image: CPP },
+      // { name: "Java", image: Java },
+      { name: "JavaScript", image: JavaScript },
+      // { name: "Python", image: Python }
+    ]
+  },
+
+  {
+    title: "Others",
+    skills: [
+      { name: "Git", image: Git },
+      { name: "Github", image: Github },
+      { name: "Microsoft Azure", image: Azure },
+      { name: "VS Code", image: VSCode },
+      { name: "Postman", image: Postman },
+      { name: "Figma", image: Figma }
+    ]
+  }
+];
+
+
+// export const experiences = [
+//   {
+//     img: FoundationalAI,
+//     role: "Frontend Developer Intern",
+//     company: "Foundational AI",
+//     date: "Dec 2023 - May 2024",
+//     desc: "During my internship at Foundational AI, I had developed and maintained end-to-end functionality for Foundational AI's SAAS products, including a CRM Outlook integration and internal application management tools. I Have Integrated various APIs to enable seamless data flow and functionality within the applications.",
+//     skills: ["HTML", "CSS", "JavaScript", "React JS", "Redux", "Next JS", "GSAP", "SASS"],
+//     doc: ""
+//   },
+//   {
+//     img: Webexcep,
+//     role: "Frontend Developer Intern",
+//     company: "Webexcep",
+//     date: "Sep 2023 - Nov 2023",
+//     desc: "During my internship at Webexcep, I had the opportunity to gain hands-on experience in frontend development. I have developed and maintained responsive websites for real-life businesses, contributing to their online presence and customer engagement.",
+//     skills: ["HTML", "CSS", "JavaScript", "React JS", "SASS", "Bootstrap", "Tailwind CSS", "Material UI"],
+//     doc: ""
+//   },
+//   {
+//     img: GirlScript,
+//     role: "Open Source Contributor",
+//     company: "GirlScript Summer of Code",
+//     date: "May 2023 - Aug 2023",
+//     desc: "I had the privilege of participating in GirlScript Summer of Code, a prestigious open source program that encourages young talents to contribute to various open source projects.",
+//     skills: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+//     doc: ""
+//   },
+//   {
+//     img: Hack2Skill,
+//     role: "Open Source Contributor",
+//     company: "Hack2Skill",
+//     date: "Jun 2023 - Jul 2023",
+//     desc: "I had the opportunity to contribute as a frontend developer in the open source community of Hack2Skill, a platform dedicated to advancing cybersecurity and ethical hacking through technology.",
+//     skills: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+//     doc: ""
+//   }
+// ];
+
+
+export const education = [
+  {
+    img: CSMU,
+    school: "Chhatrapati Shivaji Maharaj University",
+    date: "Sep 2023 - Jun 2025",
+    grade: "75%",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science at Chhatrapati Shivaji Maharaj University, Panvel, where I am honing my skills in computer science and information technology. This program is equipping me with the knowledge and expertise required to thrive in the dynamic field of technology and software development.",
+    degree: "BSc.CS (Bachelor of Science in Computer Science.)"
+  },
+
+];
+
+
+export const projects = [
+  {
+    title: "StudyZone Learning Website",
+    description: "'StudyZone' is a Real-World Educational Website that offers a comprehensive learning experience for users. 'StudyZone' is a fully responsive website developed using HTML, CSS, and JavaScript. It has 4 dynamic pages including Home, About, Courses, and Contact. Regardless of the device you use, be it a desktop computer, tablet, or smartphone, 'StudyZone' ensures a smooth browsing experience.",
+    image: StudyZone,
+    tags: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/Itsbitscoder/StudyZone",
+    link: "https://studyzone42.vercel.app/index.html",
+  },
+  // {
+  //   title: "Anshul-Estate WebApp",
+  //   description: "Developed a fully responsive Real-Estate web application using Vite JS, Node JS, Express JS, MongoDB, Firebase, Redux, Tailwind CSS. Implemented JWT for secure authentication and bcryptjs for password encryption, enhancing the security and integrity of user data. Utilized Firebase for user authentication and MongoDB for efficient and scalable data storage, ensuring robust backend support.",
+  //   image: AnshulEstate,
+  //   tags: ["Vite JS", "Node JS", "Express JS", "MongoDB", "Firebase", "Redux", "Tailwind CSS"],
+  //   github: "https://github.com/anshulgupta48/Anshul-Estate-WebApp",
+  //   link: "https://mern-estate-webapp.onrender.com/",
+  // },
+  // {
+  //   title: "Apple IPhone Website",
+  //   description: "Developed a Apple IPhone Website Clone that offers a Unique and Engaging Experience to their users. It Has 5 Sections - Hero, Highlights, IPhone Model, Features, How it Works Section. It is a fully responsive website developed using Vite JS, Tailwind CSS, GSAP and Three JS.. Regardless of the device you use, be it a desktop computer, tablet, or smartphone, it ensures a smooth browsing experience.",
+  //   image: AppleIPhone,
+  //   tags: ["Vite JS", "Tailwind CSS", "GSAP", "Three JS"],
+  //   github: "https://github.com/anshulgupta48/Apple-IPhone-Clone",
+  //   link: "https://apple-iphone-website-one.vercel.app/",
+  // },
+  // {
+  //   title: "Hoobank Website",
+  //   description: "Developed a Real-World Bank Website named 'Hoobank' that offers a Unique and Engaging Experience to their users. It Has 4 Sections - Home, Features, Products & Clients Section. 'Hoobank' is a fully responsive website developed using Vite JS and Tailwind CSS. Regardless of the device you use, be it a desktop computer, tablet, or smartphone, 'Hoobank' ensures a smooth browsing experience.",
+  //   image: HooBank,
+  //   tags: ["Vite JS", "Tailwind CSS"],
+  //   github: "https://github.com/anshulgupta48/Hoobank-Website",
+  //   link: "https://anshulgupta48.github.io/Hoobank-Website",
+  // },
+  // {
+  //   title: "GPT3-OpenAI Website",
+  //   description: "'GPT3-OpenAI' is a Real-World AI-Tool Website that offers you a collection of informative articles that dive deep into the world of AI. It Has 5 Sections - Home, What is GPT3, Features, Possibility & Blogs Section from Where is Created to Provide a Unique and Engaging experience. 'GPT3-OpenAI' is a fully responsive website developed using React JS and Sass. Regardless of the device you use, be it a desktop computer, tablet, or smartphone, 'GPT3-OpenAI' ensures a smooth browsing experience.",
+  //   image: Gpt3,
+  //   tags: ["React JS", "Sass"],
+  //   github: "https://github.com/anshulgupta48/GPT3-OpenAI-Website",
+  //   link: "https://anshulgupta48.github.io/GPT3-OpenAI-Website",
+  // },
+
+];
